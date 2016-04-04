1761fc46a789f126e73335cfe9185d1ee941f0d8
serkandurusoy <serkan.durusoy@dna-tr.com>
2016-04-04 18:50:17 +0300

    minor duzenleme

--- packages/m-lib-models/gridfs/server/permission.js
+++ packages/m-lib-models/gridfs/server/permission.js
@@ -1,7 +1,12 @@
 Security.permit([ 'insert' ]).collections([ M.FS.KurumLogo ]).ifLoggedIn().userHasRole('mitolojix').allowInClientCode();
 Security.permit([ 'update' ]).collections([ M.FS.KurumLogo ]).ifLoggedIn().userHasRole('mitolojix').allowInClientCode();
 Security.permit([ 'remove' ]).collections([ M.FS.KurumLogo ]).never().allowInClientCode();
-Security.permit([ 'download' ]).collections([ M.FS.KurumLogo ]).allowInClientCode();
+//Security.permit([ 'download' ]).collections([ M.FS.KurumLogo ]).allowInClientCode();
+M.FS.KurumLogo.allow({
+  download: function(userId, fileObj) {
+    return true;
+  }
+});
 
 
 Security.permit([ 'insert' ]).collections([ M.FS.Avatar ]).ifLoggedIn().userHasRole('mitolojix').allowInClientCode();
@@ -13,7 +18,12 @@ Security.permit([ 'update' ]).collections([ M.FS.Avatar ]).ifLoggedIn().userHasR
 Security.permit([ 'insert' ]).collections([ M.FS.Avatar ]).ifLoggedIn().userHasRole('ogretmen').allowInClientCode();
 Security.permit([ 'update' ]).collections([ M.FS.Avatar ]).ifLoggedIn().userHasRole('ogretmen').allowInClientCode();
 Security.permit([ 'remove' ]).collections([ M.FS.Avatar ]).never().allowInClientCode();
-Security.permit([ 'download' ]).collections([ M.FS.Avatar ]).ifLoggedIn().allowInClientCode();
+//Security.permit([ 'download' ]).collections([ M.FS.Avatar ]).ifLoggedIn().allowInClientCode();
+M.FS.Avatar.allow({
+  download: function(userId, fileObj) {
+    return true;
+  }
+});
 
 
 Security.permit([ 'insert' ]).collections([ M.FS.DersIcerik ]).ifLoggedIn().userHasRole('mitolojix').allowInClientCode();
@@ -23,8 +33,12 @@ Security.permit([ 'update' ]).collections([ M.FS.DersIcerik ]).ifLoggedIn().user
 Security.permit([ 'insert' ]).collections([ M.FS.DersIcerik ]).ifLoggedIn().userHasRole('ogretmen').allowInClientCode();
 Security.permit([ 'update' ]).collections([ M.FS.DersIcerik ]).ifLoggedIn().userHasRole('ogretmen').allowInClientCode();
 Security.permit([ 'remove' ]).collections([ M.FS.DersIcerik ]).never().allowInClientCode();
-Security.permit([ 'download' ]).collections([ M.FS.DersIcerik ]).ifLoggedIn().allowInClientCode();
-
+//Security.permit([ 'download' ]).collections([ M.FS.DersIcerik ]).ifLoggedIn().allowInClientCode();
+M.FS.DersIcerik.allow({
+  download: function(userId, fileObj) {
+    return !!userId;
+  }
+});
 
 Security.permit([ 'insert' ]).collections([ M.FS.SoruGorsel ]).ifLoggedIn().userHasRole('mitolojix').allowInClientCode();
 Security.permit([ 'update' ]).collections([ M.FS.SoruGorsel ]).ifLoggedIn().userHasRole('mitolojix').allowInClientCode();
@@ -33,16 +47,29 @@ Security.permit([ 'update' ]).collections([ M.FS.SoruGorsel ]).ifLoggedIn().user
 Security.permit([ 'insert' ]).collections([ M.FS.SoruGorsel ]).ifLoggedIn().userHasRole('ogretmen').allowInClientCode();
 Security.permit([ 'update' ]).collections([ M.FS.SoruGorsel ]).ifLoggedIn().userHasRole('ogretmen').allowInClientCode();
 Security.permit([ 'remove' ]).collections([ M.FS.SoruGorsel ]).never().allowInClientCode();
-Security.permit([ 'download' ]).collections([ M.FS.SoruGorsel ]).ifLoggedIn().allowInClientCode();
-
+//Security.permit([ 'download' ]).collections([ M.FS.SoruGorsel ]).ifLoggedIn().allowInClientCode();
+M.FS.SoruGorsel.allow({
+  download: function(userId, fileObj) {
+    return !!userId;
+  }
+});
 
 Security.permit([ 'insert' ]).collections([ M.FS.Muhur ]).ifLoggedIn().userHasRole('mitolojix').allowInClientCode();
 Security.permit([ 'update' ]).collections([ M.FS.Muhur ]).ifLoggedIn().userHasRole('mitolojix').allowInClientCode();
 Security.permit([ 'remove' ]).collections([ M.FS.Muhur ]).never().allowInClientCode();
-Security.permit([ 'download' ]).collections([ M.FS.Muhur ]).allowInClientCode();
-
+//Security.permit([ 'download' ]).collections([ M.FS.Muhur ]).allowInClientCode();
+M.FS.Muhur.allow({
+  download: function(userId, fileObj) {
+    return true;
+  }
+});
 
 Security.permit([ 'insert' ]).collections([ M.FS.Karakter ]).ifLoggedIn().userHasRole('mitolojix').allowInClientCode();
 Security.permit([ 'update' ]).collections([ M.FS.Karakter ]).ifLoggedIn().userHasRole('mitolojix').allowInClientCode();
 Security.permit([ 'remove' ]).collections([ M.FS.Karakter ]).never().allowInClientCode();
-Security.permit([ 'download' ]).collections([ M.FS.Karakter ]).allowInClientCode();
+//Security.permit([ 'download' ]).collections([ M.FS.Karakter ]).allowInClientCode();
+M.FS.Karakter.allow({
+  download: function(userId, fileObj) {
+    return true;
+  }
+});
