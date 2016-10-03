40f9b4ce2f2d5109f545b973e4bfe63a3beef187
serkandurusoy <serkan.durusoy@dna-tr.com>
2016-10-03 17:06:15 +0300

    tab basliginda ortamin ismini goster

--- main.js
+++ main.js
@@ -3,5 +3,8 @@ Meteor.startup(function() {
     console.log("======================================");
     console.log("Meteor client for " + Meteor.settings.public.APP + " started.");
     console.log("======================================");
+
+    document.title = 'Mitolojix' + ( Meteor.settings.public.ENV === 'PRODUCTION' ? ' ' : (' ' + Meteor.settings.public.ENV + ' ') ) + 'Yönetim Arayüzü';
+
   }
 });
