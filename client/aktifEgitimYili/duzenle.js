62ca26e34bc15b6c56bfaa72d7e55cc3f4d0d6c1
cyardimci <cyardimci@sabanciuniv.edu>
2016-10-20 01:40:52 +0300

    #518 aktifEgitimYili kod mozerniasyonu  client tarafi  duzenle.js pushlanmamis. Pushlandi.

--- client/aktifEgitimYili/duzenle.js
+++ client/aktifEgitimYili/duzenle.js
@@ -1,12 +1,12 @@
 Template.aktifEgitimYili.helpers({
-  aktifEgitimYili: function() {
+  aktifEgitimYili() {
     return M.C.AktifEgitimYili.findOne();
   }
 });
 
 AutoForm.hooks({
   aktifEgitimYiliDuzenleForm: {
-    onSuccess: function(operation, result, template) {
+    onSuccess(operation, result, template) {
       if (result) {
         toastr.success('Aktif eğitim yılı değiştirildi.');
       }
