d069bcd8f06cd14d309a3c0e5cc05612c77ed6b1
cyardimci <cyardimci@sabanciuniv.edu>
2016-11-19 22:48:50 +0300

    #518 soruFavori klasoru import sistemine uygun olacak seklde guncellendi

--- packages/m-lib-models/soruFavori/server/_search.js
+++ packages/m-lib-models/soruFavori/server/_search.js
@@ -1,3 +1,10 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+
+import { _ } from 'meteor/underscore';
+
+import { M } from 'meteor/m:lib-core';
+
 Meteor.methods({
   'search.soruFavori'(keywords, filters) {
     check(keywords, Match.OneOf(undefined, null, String));
---
+++ packages/m-lib-models/soruFavori/server/index.js
@@ -0,0 +1,2 @@
+import './_publish';
+import './_search';
