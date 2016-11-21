5992dc1b719acc6de36bf8621e3e2ef5dbcbf7cf
cyardimci <cyardimci@sabanciuniv.edu>
2016-11-21 12:38:00 +0300

    #518 client/bilgilerim klasoru import  sistemine uygun olacak sekilde guncellendi

--- client/bilgilerim/detay.js
+++ client/bilgilerim/detay.js
@@ -1,3 +1,17 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
+import { check } from 'meteor/check';
+import { _ } from 'meteor/underscore';
+import { $ } from 'meteor/jquery';
+
+import { FS } from 'meteor/cfs:collection';
+
+import { M } from 'meteor/m:lib-core';
+
+import './detayKart.html';
+
 Template.bilgilerimDetayKart.onRendered(function(){
   this.parent().$('.collapsible').collapsible();
   this.$('[data-tooltip]').tooltip({delay: 50, position: 'bottom'});
--- client/bilgilerim/tarihce.js
+++ client/bilgilerim/tarihce.js
@@ -1,3 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+import { Blaze } from 'meteor/blaze';
+import { Template } from 'meteor/templating';
+
+import './tarihce.html';
+
 Template.bilgilerimTarihce.helpers({
   bilgilerimTarihce() {
     return Blaze._globalHelpers.$mapped(Meteor.user().versions());
