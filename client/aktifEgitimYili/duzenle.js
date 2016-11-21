611df41b95674c8f7ada816f27bd61077e1776bb
cyardimci <cyardimci@sabanciuniv.edu>
2016-11-21 12:30:06 +0300

    #518 client/aktifEgitimYili klasoru import  sistemine uygun olacak sekilde guncellendi

--- client/aktifEgitimYili/duzenle.js
+++ client/aktifEgitimYili/duzenle.js
@@ -1,3 +1,11 @@
+import { Template } from 'meteor/templating';
+
+import { AutoForm } from 'meteor/aldeed:autoform';
+
+import { M } from 'meteor/m:lib-core';
+
+import './duzenle.html';
+
 Template.aktifEgitimYili.helpers({
   aktifEgitimYili() {
     return M.C.AktifEgitimYili.findOne();
